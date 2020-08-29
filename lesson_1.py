9061cffad72b2305972bde84001b97127586b391
David LeBauer <dlebauer@arizona.edu>
2020-08-29 11:38:19 -0700

    Create lesson_1.py

---
+++ lesson_1.py
@@ -0,0 +1,34 @@
+# operators +, -, *, -, (,), round
+1+2
+1-2
+1*2
+1/2
+round(1/2)
+1 + 2 * 3
+(1 + 2) * 3
+
+
+# logical
+1 > 2  # False
+1 == 2 # False
+
+# Variables
+
+a = 1
+b = 2
+a + b
+
+# Strings
+
+fred = "Why do gorillas have big nostrils? Big fingers!!"
+print(fred)
+
+# lists
+
+wizard_list = ['spider legs', 'toe of a frog', 'eye of a newt', 'cauldron']
+wizard_list.append('snake burp')
+
+# notice that Python (like Scratch) starts counting from 0 instead of 1!
+print(wizard_list[2])
+
+
