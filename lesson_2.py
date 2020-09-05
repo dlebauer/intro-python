b658eb14bc37d6a453824fb7558e7f7fd1105e41
David LeBauer <dlebauer@arizona.edu>
2020-09-05 10:00:01 -0700

    Create lesson_2.py

---
+++ lesson_2.py
@@ -0,0 +1,48 @@
+# review last week
+# operators
+# logical
+# variable names - no spaces!
+
+# who did math homework?
+# quiz: apples cost $1 and oranges cost $1.50
+# create a variable for apples and one for oranges
+# calculate the cost of 5 apples and 3 oranges
+# how many oranges can you buy for $9? for $10?
+
+## New
+print("Hello world")
+
+## String Substitution
+# my name is Pat
+myname = 'Pat'
+greeting = 'Hello %s'
+print(greeting % myname)
+
+greeting = 'Hello %s'
+names = ['Jens', 'Anya', 'Emil', 'Diza', 'Kalil']
+print(greeting % names[1])
+
+for x in names:
+   print(x)
+   
+
+# for loops
+for x in names:
+   print(greeting % x)
+   
+
+# for loops
+for x in names:
+  print(greeting % x)
+  
+
+
+for x in names[1:2]:
+  print(greeting % x)
+  
+
+# 
+# for loops
+for x in (0,1):
+  print(greeting % names[x])
+   
