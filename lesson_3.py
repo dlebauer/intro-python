397e2d003dfff75be787b99b3d2e558cae006fc0
David LeBauer <dlebauer@arizona.edu>
2020-09-12 09:44:31 -0700

    Create lesson_3.py

---
+++ lesson_3.py
@@ -0,0 +1,11 @@
+import turtle
+
+tina = turtle.Turtle()
+#tina.shape('turtle')
+tina.write("Hello world")
+tina.write('Hello there')
+tina.forward(200)
+#for i in range(0,4):
+#  tina.forward(20)
+#  tina.left(90)
+  
