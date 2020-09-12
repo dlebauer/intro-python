6b4a40af41d86d8ef98f881f401dc52fba8e1aa3
David LeBauer <dlebauer@arizona.edu>
2020-09-12 11:04:09 -0700

    Update lesson_3.py

--- lesson_3.py
+++ lesson_3.py
@@ -1,11 +1,47 @@
 import turtle
 
 tina = turtle.Turtle()
-#tina.shape('turtle')
 tina.write("Hello world")
-tina.write('Hello there')
+
+tina.forward(50)
+tina.right(45)
+tina.forward(50)
+
+tina.clear()
+
+#tina.shape('turtle')
 tina.forward(200)
 #for i in range(0,4):
 #  tina.forward(20)
 #  tina.left(90)
   
+
+tony = turtle.Turtle()
+
+
+
+tony.color('red')
+tony.forward(30)
+tony.left(120)
+tony.forward(30)
+tony.left(120)
+tony.forward(30)
+
+tina.clear()
+tina.color('green')
+tina.penup()
+tina.goto(100, 100)
+tina.pendown()
+tina.goto(-100, 100)
+tina.goto(-100, -100)
+tina.goto(100, -100)
+tina.goto(100, 100)
+
+
+
+
+tony.color('red')
+for i in range(0, 100):
+  tony.forward(30+i)
+  tony.left(30)
+
