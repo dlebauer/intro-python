b45c098c2e892e5303e7d60da333d42ee29261ec
David LeBauer <dlebauer@arizona.edu>
2020-09-26 13:26:34 -0700

    Create lesson_4_add_images.py

---
+++ lesson_4_add_images.py
@@ -0,0 +1,12 @@
+import turtle
+
+
+tom = turtle.Turtle()
+
+screen = turtle.Screen()
+screen.setup(256,256)
+screen.bgpic('tucson.jpg')
+
+screen.addshape("cactus.png")
+tom.shape("cactus.png")
+tom.left(90)
