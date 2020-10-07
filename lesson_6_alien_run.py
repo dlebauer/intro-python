92f36a968508560a41cc1fd2e8b7f5dd5cf8275c
David LeBauer <dlebauer@arizona.edu>
2020-10-07 13:25:37 -0700

    Create lesson_6_alien_run.py

---
+++ lesson_6_alien_run.py
@@ -0,0 +1,36 @@
+import pgzrun
+
+WIDTH = 300
+HEIGHT = 300
+
+def draw():
+    screen.fill((128, 0, 0))
+
+
+alien = Actor('alien')
+alien.pos = 100, 56
+alien.topright = 0, 10
+
+
+WIDTH = 500
+HEIGHT = alien.height + 20
+
+def draw():
+    screen.clear()
+    alien.draw()
+
+
+def update():
+    alien.left += 2
+    if alien.left > WIDTH:
+        alien.right = 0
+
+def on_mouse_down(pos):
+    if alien.collidepoint(pos):
+        print("Eek!")
+    else:
+        print("You missed me!")
+
+
+# last line
+pgzrun.go()
