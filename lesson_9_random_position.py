dd29bcb4c6682efa8dfeee5a0d2c75beb28f1d7f
David LeBauer <dlebauer@arizona.edu>
2020-10-24 11:22:16 -0700

    Update lesson_9_random_position.py

--- lesson_9_random_position.py
+++ lesson_9_random_position.py
@@ -1,32 +1,27 @@
-# from https://pygame-zero.readthedocs.io/en/stable/introduction.html
 import pgzrun
 import random
 
-WIDTH = 300
-HEIGHT = 300
+WIDTH = 800
+HEIGHT = 600
 
+alien = Actor("alien")
+alien.pos = WIDTH / 2, HEIGHT / 2
 
-alien = Actor('alien')
-alien.pos = 100, 56
-alien.topright = 0, 10
-
-
-WIDTH = 500
-HEIGHT = 1000
 
 def draw():
     screen.clear()
     screen.fill((128, 0, 0))
+    # screen.fill((random.randint(0,255), random.randint(0,255), random.randint(0,255)))
     alien.draw()
 
+
 def update():
-    alien.left += 20
-    if alien.left > WIDTH:
+    alien.x += random.randint(1, 20)
+    alien.y += random.randint(-10, 10)
+    if alien.x > WIDTH:
         alien.x = 0
-        alien.y = random.randint(1,1000)
+        alien.y = random.randint(0, HEIGHT)
+    if alien.y > HEIGHT:
+        alien.y = 0
 
-
-
-
-# last line
 pgzrun.go()
